adffdf9e27290ee5a20b331111f76c281864d893
Gabriel Zanardi <gabrielzanardi805@gmail.com>
2026-06-09 21:49:18 -0300

    Atualização

--- cypress/e2e/Credenciais validas.cy.js
+++ cypress/e2e/Credenciais validas.cy.js
@@ -1,22 +1,13 @@
-describe.only('fluxo de autenticação', () => {
+describe('fluxo de autenticação', () => {
   it('Exibir tela de inventário após login bem-sucedido', () => {
+    // Passo 1: Visitar a URL do site
     cy.visit('https://front.serverest.dev/login')
-cy.get('[data-testid="email"]').type('gabriel@qa.com.br')
-cy.get('[data-testid="senha"]').type('teste')
-cy.get('[data-testid="senha"]').should('have.value', 'teste')
+      // Passo 2: Preencher os campos de login
+cy.get('[data-testid="email"]').type('gabriel@portfolio.com.br')
+cy.get('[data-testid="senha"]').type('1234')
+cy.get('[data-testid="senha"]').should('have.value', '1234')
 cy.get('[data-testid="entrar"]').click()    
 cy.get('h1').should('have.text', 'Serverest Store')
-
-
-
-
-
   })
 })  
-    
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://front.serverest.dev/login')
-  })
-})
