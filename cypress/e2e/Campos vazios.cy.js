4a0b0006cf25ce6fc15d153d126e4da6b0b76b3d
Gabriel Zanardi <gabrielzanardi805@gmail.com>
2026-06-09 21:56:25 -0300

    Atualização

--- cypress/e2e/Campos vazios.cy.js
+++ cypress/e2e/Campos vazios.cy.js
@@ -1,9 +1,11 @@
 describe('fluxo de autenticação', () => {
   it('exibir mensagem de erro para campos vazios e-mail e senha obrigatórios', () => {
+    // Passo 1: Acessar a página de login
     cy.visit('https://front.serverest.dev/login')
+    // Passo 2: clicar no botão entrar sem preencher os campos de login
     cy.get('[data-testid="entrar"]').click()
+    // Passo 3: Verificar as mensagens de erro para campos vazios
     cy.get(':nth-child(4) > :nth-child(2)').should('have.text', 'Password é obrigatório')
     cy.get(':nth-child(3) > :nth-child(2)').should('have.text', 'Email é obrigatório')
-  
   })
 })
